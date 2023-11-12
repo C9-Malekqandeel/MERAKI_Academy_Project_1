62774573ebf3f239bd1d9d0f72154421db54ce04
Malekqandeel <malikqan@gmail.com>
2023-11-13 00:25:48 +0300

    CSS was done, will prceed with coding NOW

--- app.js
+++ app.js
@@ -3,3 +3,23 @@ const boxPlay = [
     [],
     []
 ]
+
+const row = ["","" ,"", "", "", "","","",""];
+const element= document.querySelectorAll('.Box')
+const status = document.querySelector('#status')
+const restart = document.querySelector('#restart')
+let player1 ="<img src='./X Letter.png'>"
+let player2 ="<img src='./O Letter.png'>"
+
+
+console.log(element);
+element.forEach((elem,i)=> {
+    elem.addEventListener('click', addPicture)
+}
+
+
+
+)
+function boxClick (){
+
+}
