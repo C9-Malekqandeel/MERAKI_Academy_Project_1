56d450a0f22becfbb619a428dd304ee60af6e203
Malekqandeel <malikqan@gmail.com>
2023-11-13 10:04:51 +0300

    some of updates was made

--- app.js
+++ app.js
@@ -4,22 +4,25 @@ const boxPlay = [
     []
 ]
 
-const row = ["","" ,"", "", "", "","","",""];
 const element= document.querySelectorAll('.Box')
 const status = document.querySelector('#status')
 const restart = document.querySelector('#restart')
-let player1 ="<img src='./X Letter.png'>"
-let player2 ="<img src='./O Letter.png'>"
 
+const player1 = new Image (100,100);
+player1.src="X Letter.png";
+const player2 = new Image (100 ,100);
+player2.src="O Letter.png"
+
+const picture_O = '<img src="./O Letter.png">'
+
+const addPicture = ()=> {
+    const box = document.querySelector('.Box')
+    console.log(box);
+        box.textContent = picture_O
+    
+}
 
 console.log(element);
-element.forEach((elem,i)=> {
+element.forEach(elem => {
     elem.addEventListener('click', addPicture)
-}
-
-
-
-)
-function boxClick (){
-
-}
+});
