b6125e681e0e8bb9f934b0e21d773d6fb51ba85b
Malekqandeel <malikqan@gmail.com>
2023-11-13 22:04:20 +0300

    Some updates

--- app.js
+++ app.js
@@ -37,9 +37,9 @@ const changeplay = ()=>{
 
 const checkWin = (boxPlay)=>{
 
-    console.log(boxPlay);
+    //console.log(boxPlay);
    for (let index = 0; index < boxPlay.length; index++) {
-    console.log(boxPlay[index][1]);
+    //console.log(boxPlay[index][1]);
         if (boxPlay[index][0] === boxPlay[index][1] && boxPlay[index][1] === boxPlay[index][2] && boxPlay[index][0] !== ""){
             document.querySelector('#status').innerText = changeplay() + "    is Win"
             continuePlay = true
@@ -56,15 +56,35 @@ const checkWin = (boxPlay)=>{
     } else if ( boxPlay[0][2] === boxPlay[1][1]&& boxPlay[1][1] === boxPlay[2][0] && boxPlay[0][2]!==""){
         document.querySelector('#status').innerText = changeplay() + "    is Win"
             continuePlay = true
+
     }
     
 }
 
 
+const restart = document.querySelector('#restart')
+restart.addEventListener('click',()=>{
+          let box = document.querySelectorAll('.Box')
+          box.forEach(element => {
+            element.innerText = ""
+          });
+          document.querySelector('#status').innerText = "Keep fighting !"
+    });
 
 
 
 render(boxPlay)
+
+
+
+// last funtion will be created for more features,
+// transfore to new screen will one win!
+// Screen contains the name of player + button to play again
+
+// You can just dis-click on the box when containPlay false!
+
+
+
 
 
 
@@ -73,26 +93,22 @@ render(boxPlay)
 // [0][1]===[1][1]===[2][1]
 // [0][2]===[1][2]===[2][2]
 
-
-
-
-
 // const block = document.createElemnt("div")
 // block.classList.add("Box")
 
 //const element= document.querySelectorAll('.Box')
-const status = document.querySelector('#status')
+/* const status = document.querySelector('#status')
 const restart = document.querySelector('#restart')
 
 
 
 const picture_O = ' <img src="./O Letter.png"> '
-/* 
+/*  *//* 
 const addPicture = ()=> {
     const box = document.querySelector('.Box')
     console.log(box);
     console.log(player1);
- */
+ */ 
 /*         box.textContent = picture_O
  */    
 
